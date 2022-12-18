2508c6c3d83ebfea22d45c2b77b8d719479a2f58
Anthony Ladon <ladonanthony@gmail.com>
2022-12-18 12:39:23 +0100

    ajout css media queries navbar + logo

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+// gestion de l'annimation menu hamburger (@media-querie Smartphone)
+let icon = document.getElementById("icon");
+let icon1 = document.getElementById("a");
+let icon2 = document.getElementById("b");
+let icon3 = document.getElementById("c");
+let nav = document.getElementById("nav");
+let blue = document.getElementById("blue");
 
-
-
+icon.addEventListener("click", function () {
+  icon1.classList.toggle("a");
+  icon2.classList.toggle("c");
+  icon3.classList.toggle("b");
+  nav.classList.toggle("show");
+  blue.classList.toggle("slide");
+});
