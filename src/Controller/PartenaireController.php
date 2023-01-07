34812567e028c9a2f6d76cce7e0abbdc858f1312
Anthony Ladon <ladonanthony@gmail.com>
2023-01-08 00:05:46 +0100

    modification titre principal pour etre utiilisé partout

--- src/Controller/PartenaireController.php
+++ src/Controller/PartenaireController.php
@@ -22,43 +22,4 @@ class PartenaireController extends AbstractController
             "partenaires" => $partenaires,
         ]);
     }
-
-
-
-    /**
-     * @Route("article/ajout")
-     */
-
-    // public function new(EntityManagerInterface $entityManager): Response
-    // {
-
-    //     $faker = Factory::create();
-    //     $article = new Article;
-    //     $article->setTitre($faker->title);
-    //     $article->setContenu($faker->text($maxNbChars = 200));
-    //     $article->setDate($faker->dateTimeBetween($startDate = '-5 years', $endDate = 'now', $timezone = "Europe/Paris"));
-
-    // $article->setTitre('Mon article 1');
-    // $article->setContenu('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
-    // $article->setDate(new DateTime("2015-03-14 00:00"));
-
-    // $article = new Article();
-    // $article->setTitre('consectetur adipiscing');
-    // $article->setContenu('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut magique labore et dolore magna. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
-    // $article->setDate(new DateTime("2018-01-01 00:00"));
-
-    // $article = new Article();
-    // $article->setTitre('Labore et dolore');
-    // $article->setContenu('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
-    // $article->setDate(new DateTime("2021-01-01 00:00"));
-
-    // $entityManager->persist($article);
-    // $entityManager->flush();
-
-    // Pour effacer:
-    // $entityManager->remove($article);
-    // $entityManager->flush();
-
-    //     return new Response("Article bien envoyé en base de donnée");
-    // }
 }
