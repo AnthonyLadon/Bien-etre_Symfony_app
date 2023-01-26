0f6194e00fef35294e1979310df2fe25409e3ec8
Anthony Ladon <ladonanthony@gmail.com>
2023-01-26 20:42:50 +0100

    ajout requete ajax fichier json codepostaux belgique

--- public/js/main.js
+++ public/js/main.js
@@ -14,16 +14,46 @@ icon.addEventListener("click", function () {
   blue.classList.toggle("slide");
 });
 
+//requete AJAX formulaire de recherche pour récupérer villes et codes postaux de Belgique
+const LOCALITE = document.getElementById("prestataire_search_localite");
+const CODEPOSTAL = document.getElementById("prestataire_search_cp");
+const COMMUNE = document.getElementById("prestataire_search_commune");
+
+CODEPOSTAL.addEventListener("change", function () {
+  let url = "../zipcode-belgium.json";
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.send(null);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+      // récupération de la valeur (Numéro du select) du code postal selectionné
+      let num = CODEPOSTAL.value;
+      // Puis la valeur du texte dans le select
+      let selectedZip = CODEPOSTAL[num].innerText;
+
+      // traitement des données reçues
+      let datas = JSON.parse(xhr.response);
+
+      for (data of datas) {
+        if (selectedZip == data.zip) {
+          console.log(selectedZip + " -> " + data.city);
+        }
+      }
+    }
+  };
+});
+
 // Permet, sur les autres pages qu'accueil, d'afficher et de masquer le zone de recherche
-const targetDiv = document.getElementById("search-bar-wrap");
-const btn = document.getElementById("search-button-toggle");
-targetDiv.style.display = "none";
-btn.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
-    btn.innerHTML = '<i class="fa fa-search"></i>';
+const TargetDiv = document.getElementById("search-bar-wrap");
+const BTN = document.getElementById("search-button-toggle");
+TargetDiv.style.display = "none";
+BTN.onclick = function () {
+  if (TargetDiv.style.display !== "none") {
+    TargetDiv.style.display = "none";
+    BTN.innerHTML = '<i class="fa fa-search"></i>';
   } else {
-    targetDiv.style.display = "block";
-    btn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    TargetDiv.style.display = "block";
+    BTN.innerHTML = '<i class="fa-solid fa-xmark"></i>';
   }
 };
