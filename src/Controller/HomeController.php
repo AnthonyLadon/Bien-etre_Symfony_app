28ca75de902554d30ec37b8d64ac1cfa82b60bdf
Anthony Ladon <ladonanthony@gmail.com>
2023-02-19 23:07:54 +0100

    modif formulaire edit profil user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -84,3 +84,24 @@ class HomeController extends AbstractController
     }
 
 }
+
+
+    // try
+    // {
+    //     // On se connecte à MySQL
+    //     $mysqlClient = new PDO('mysql:host=localhost;dbname=my_recipes;charset=utf8', 'root', 'root');
+    // }
+    // catch(Exception $e)
+    // {
+    //     // En cas d'erreur, on affiche un message et on arrête tout
+    //         die('Erreur : '.$e->getMessage());
+    // }
+    
+    // // Si tout va bien, on peut continuer
+    
+    // // On récupère tout le contenu de la table recipes
+    // $sqlQuery = 'SELECT * FROM recipes';
+    // $recipesStatement = $mysqlClient->prepare($sqlQuery);
+    // $recipesStatement->execute();
+    // $recipes = $recipesStatement->fetchAll();
+    
--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -2,10 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\CodePostal;
+use App\Entity\Localite;
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UtilisateurType extends AbstractType
@@ -13,22 +19,27 @@ class UtilisateurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
             // ->add('roles')
             // ->add('password')
-            ->add('adresseNum')
-            ->add('adresseRue')
+            ->add('adresseRue', TextType::class)
+            ->add('adresseNum', NumberType::class)
             // ->add('dateInscription')
             // ->add('typeUtilisateur')
-            ->add('nomComplet')
-            ->add('localite')
-            ->add('codePostal')
+            // ->add('nomComplet', TextType::class)
+            ->add('localite', EntityType::class,[
+                'class' => Localite::class,
+                'required' => false,
+                'placeholder' => '-- Veuillez choisir une catégorie --'])
+            ->add('codePostal', EntityType::class,[
+                'class' => CodePostal::class,
+                'required' => false,
+                'placeholder' => '-- Veuillez choisir un code postal --'])
+
             ->add('commune')
-            // ->add('internautes')
             ->add('modifier', SubmitType::class, 
             ['label' => 'Enregistrer modifications']
-            )
-        ;
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
